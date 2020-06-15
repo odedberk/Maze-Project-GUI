6579e562ed8df8d05fe0dc12b52fe337b8305d69
eilam gal <eilamg@post.bgu.ac.il>
2020-06-15 20:02:41 +0300

    new commit

--- src/View/MyViewController.java
+++ src/View/MyViewController.java
@@ -22,6 +22,15 @@ public class MyViewController implements IView{
     private AMazeGenerator mazeGenerator;
     public MazeDisplayer mazeDisplayer;
 
+    public MyViewController( AMazeGenerator mazeGenerator, MazeDisplayer mazeDisplayer) {
+        this.mazeGenerator = mazeGenerator;
+        this.mazeDisplayer = mazeDisplayer;
+        this.mazeDisplayer.setImageFileNameWall("C:\\Users\\eilam gal\\IdeaProjects\\ATP-Project-PartC\\resources\\Images\\wall.PNG");
+        this.mazeDisplayer.setImageFileNamePlayer("C:\\Users\\eilam gal\\IdeaProjects\\ATP-Project-PartC\\resources\\Images\\\u200F\u200Fdudu.PNG");
+        this.mazeDisplayer.setImageFileGoal("C:\\Users\\eilam gal\\IdeaProjects\\ATP-Project-PartC\\resources\\Images\\fish.PNG");
+
+    }
+
     public void loadGame(ActionEvent actionEvent) {
     }
 
@@ -32,6 +41,7 @@ public class MyViewController implements IView{
     }
 
     public void showAbout(ActionEvent actionEvent) {
+        System.out.println("Git");
     }
 
     public void exitProgram(){
